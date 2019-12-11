244172d1aca56166c8d60737a55b8acc102f1f74
Steve Tice <stic6021@yahoo.com>
2019-12-11 17:17:13 +0000

    Lesson 7:v1.

---
+++ db/migrate/20191211170445_create_grams.rb
@@ -0,0 +1,10 @@
+class CreateGrams < ActiveRecord::Migration[5.2]
+  def change
+    create_table :grams do |t|
+      t.text :message
+      t.integer :user_id
+      t.string :picture
+      t.timestamps
+    end
+  end
+end
